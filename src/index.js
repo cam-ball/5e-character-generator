1b62cdd5db83cec473b3d708276b2169c13a3644
laurenball <lauren.l.dev@gmail.com>
2021-05-01 23:10:48 -0400

    Add uikit
    consistent styling across all apps in domain

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// import "uikit/dist/css/uikit.min.css"
 
 ReactDOM.render(
   <React.StrictMode>

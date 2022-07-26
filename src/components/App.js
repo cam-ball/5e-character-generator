506e89761fa049e10005f6f8c5fbcef7bbe307d8
Cameron Ball <cameronb@pvep.com>
2022-07-26 18:43:43 -0400

    Update URL

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
       </div>
     </div>
     <footer className="footer mt-auto py-3 text-center">
-      made with ♥  by <a href="https://laurenball.github.io">laurenball</a>
+      made with ♥  by <a href="https://camballsoup.dev">cam-ball</a>
     </footer>
     </>
   );

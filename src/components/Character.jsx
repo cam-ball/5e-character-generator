1f218fd6ef014438dd0a5363303084ade593ee84
cam-ball <hi@camball.me>
2026-05-17 23:22:04 -0400

    fix class name issues

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Attribute from './Attribute'
+import React from "react";
+import Attribute from "./Attribute";
 
 const Character = (props) => {
   const { characterAlignment, characterRace, characterClass } = props;
 
   return (
-    <div class='col text-center'>
-      You should build a <Attribute attribute={characterAlignment} />  <Attribute attribute={characterRace} /> <Attribute attribute={characterClass} />!
+    <div className="col text-center">
+      You should build a <Attribute attribute={characterAlignment} />{" "}
+      <Attribute attribute={characterRace} />{" "}
+      <Attribute attribute={characterClass} />!
     </div>
-  )
-}
+  );
+};
 
 export default Character;
